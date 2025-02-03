167174df6ecfeb0e336590f8429b86100031be8e
clbokea <clbo@kea.dk>
2025-02-03 16:41:00 +0100

    new tests

---
+++ src/test/java/ita3/imdb_api/ClboTest.java
@@ -0,0 +1,16 @@
+package ita3.imdb_api;
+
+import org.junit.jupiter.api.Test;
+
+public class ClboTest {
+
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    @Test
+    public void testCallName() {
+        System.out.println("Hello name");
+    }
+}
